b7c69eae0d7bb070d0feb87e689da809c1d1d736
MarcSpectrum <esnger.9@gmail.com>
2026-08-01 14:08:49 +0700

    Shoot ducking Ball

--- Assets/Scripts/Bowling.cs
+++ Assets/Scripts/Bowling.cs
@@ -41,7 +41,7 @@ public class Bowling : MonoBehaviour
         }
     }
 
-    private void ShootBall()
+    public void ShootBall()
     {
         rb.AddForce(Vector3.forward * forcePower, ForceMode.Impulse);
         hasShot = true;
